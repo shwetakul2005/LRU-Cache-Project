e09b8b9cb809cd2f5c16783017e9712147a3c520
shwetakul2005 <shwetakul2020@gmail.com>
2025-08-07 17:27:24 +0530

    updated_lru

--- LRU.cpp
+++ LRU.cpp
@@ -30,73 +30,10 @@ public:
     int max_sz=4;
 
     Node* head=nullptr;
+    Node* tail=nullptr;
     unordered_map<std::string, Node*> cache;
     //int ca_sz=cache.size();
-    
-    void put_url(string url1){
-
-        //if cache exists in the hashmap, means it just needs to be brought to the head position
-        if(cache.find(url1) != cache.end()){
-            get_url(url1);
-        }
-
-        //if cache[url] dosent exist int the hashmap, then there are 3 cases:
-        //i)either there is no element in the dll at all and this is the 1st element
-        //ii)there is space in cache to add new url as head without deleting
-        //iii)the cache space is full and the tail url must be deleted
-        else{
-            //
-            if(cache.size()==0){
-                head= new Node(url1);
-                cache[url1]=head;
-            }
-
-            else if(cache.size()>0 && cache.size()<max_sz){
-                Node* new_node=new Node(url1);
-
-                //error#1: You never allocated memory for new_node, so it causes undefined behavior when accessing url.
-                //new_node->url=url1;
-
-                cache[url1]=new_node;
-                head->back=new_node;
-                new_node->back=nullptr;
-                new_node->next=head;
-                head=new_node;
-
-                //delete new_node;
-
-            }
-
-            else if(cache.size()==max_sz){
-                Node* new_node=new Node(url1);
-                Node* mover=head;
-
-                //deleting the tail
-                while(mover->next){
-                    mover=mover->next;
-                }
-                //removes the tail from the hashmap
-                cache.erase(mover->url);
-
-                //removes the tail from the dll
-                Node* temp=mover->back;
-                temp->next=nullptr;
-                mover->back=nullptr;
-
-                //adding new url as head
-                //new_node= new Node(url1);
-                cache[url1]=new_node;
-                head->back=new_node;
-                new_node->back=nullptr;
-                new_node->next=head;
-                head=new_node;
-
-                //  delete new_node;
-                  delete mover;
-                //  delete temp;
-            }
-        }
-
+    void print(){
         for(auto it: cache){
             cout<<it.first<<endl;
         }
@@ -104,33 +41,66 @@ public:
         cout<<"------"<<endl;
     }
 
+    void movetoHead(Node* node){
+        if(node == head) return;
 
-    string get_url(string url1){
-
-        if(cache.find(url1) != cache.end()){
-            if(cache[url1]!=head){
-                Node* mover=cache[url1];
-                Node* temp1=mover->back;
-                Node* temp2=mover->next;
-                head->back=mover;
-                mover->back=nullptr;
-                mover->next=head;
-                head=mover;
-                temp1->next=temp2;
-                if(temp2) temp2->back=temp1;
-                return "yayyy!!";
-
-                  delete mover;
-                //  delete temp1;
-                //  delete temp2;
-
-            }
-            else if(cache[url1]==head){
-                return "already_head";
-            }
+        // detach node
+        if(node == tail){
+            tail=node->back;
+            tail->next=nullptr;
+        }
+        else{
+            node->back->next=node->next;
+            node->next->back=node->back;
         }
 
-        return "error!!";
+        node->next=head;
+        node->back=nullptr;
+        head->back=node;
+        head=node;
+    }
+    
+    void put_url(string url1){
+        auto it=cache.find(url1);
+
+        //if cache exists in the hashmap, means it just needs to be brought to the head position
+        if(it!=cache.end()){
+            movetoHead(it->second);
+            return;
+        }
+
+        //if cache[url] dosent exist int the hashmap, then there are 3 cases:
+        //i)either there is no element in the dll at all and this is the 1st element
+        //ii)there is space in cache to add new url as head without deleting
+        //iii)the cache space is full and the tail url must be deleted
+        Node* new_node = new Node(url1);
+        cache[url1]=new_node;
+        if(head==nullptr){
+            head=new_node;
+            tail=new_node;
+        }
+        else{
+            new_node->next=head;
+            head->back=new_node;
+            head=new_node;
+        }
+        if(cache.size()>max_sz){
+            cache.erase(tail->url);
+            Node* temp=tail;
+            tail=temp->back;
+            tail->next=nullptr;
+            delete temp;
+        }
+        print();    
+    }
+
+
+    string get_url(string url1){
+        auto it=cache.find(url1);
+        if(it==cache.end()) return "error!!!the url dosen't exist ";
+        movetoHead(it->second);
+
+        if(head->url==url1) return "yayy!!!";
     }
 };
 
@@ -150,7 +120,8 @@ obj1.put_url(u4);  // Works fine
 obj1.put_url(u5);  // Works fine
 // cout<<"enter ur url"<<endl;
 // cin>>url1;
-cout<<obj1.get_url(u);
+cout<<"input res: "<<obj1.get_url(u)<<endl;
+cout<<"input res: "<<obj1.get_url(u4);
 
 
 return 0;
